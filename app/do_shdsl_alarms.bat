740b3beba1adfe4a6c988547b5186cd4621b5462
Genildo Ferreira <gf.analyst@gmail.com>
2014-12-26 11:39:54 -0200

    Fixed the file path in the .bat files

--- app/do_shdsl_alarms.bat
+++ app/do_shdsl_alarms.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\dev\jruby-1.7.16\bin\jruby.exe --2.0 -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/RubymineProjects/nwslib/gvt/shdsl_alarms.rb
+C:\dev\jruby-1.7.16\bin\jruby.exe --2.0 -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/RubymineProjects/nwslib/app/shdsl_alarms.rb
