e7f114ce69e8d981fefee2edcc661d4d3cc86747
Genildo Ferreira <gf.analyst@gmail.com>
2016-04-30 12:29:59 -0300

    added jruby_home system variable to the path

--- app/do_infrastructure_alarms.bat
+++ app/do_infrastructure_alarms.bat
@@ -1,1 +1,1 @@
-\Bitnami\jrubystack-9.0.5.0-0\jruby\bin\jruby.exe --2.0 -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nwslib/app/infrastructure_alarms.rb
+\%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/infrastructure_alarms.rb
--- app/do_shdsl_alarms.bat
+++ app/do_shdsl_alarms.bat
@@ -1,1 +1,1 @@
-\Bitnami\jrubystack-9.0.5.0-0\jruby\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nwslib/app/shdsl_alarms.rb
+\%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/shdsl_alarms.rb
