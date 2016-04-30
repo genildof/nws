212ea189b352a4b39a73ba35c1ffef1f43e4413f
Genildo Ferreira <gf.analyst@gmail.com>
2016-04-30 01:13:23 -0300

    bat and gem files updated

--- app/do_infrastructure_alarms.bat
+++ app/do_infrastructure_alarms.bat
@@ -1,2 +1,1 @@
-@echo off
-C:\dev\jruby-1.7.16\bin\jruby.exe --2.0 -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/RubymineProjects/nwslib/app/infrastructure_alarms.rb
+\Bitnami\jrubystack-9.0.5.0-0\jruby\bin\jruby.exe --2.0 -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nwslib/app/infrastructure_alarms.rb
