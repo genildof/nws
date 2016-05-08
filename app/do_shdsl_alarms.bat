2f3707465726eb7b92c2612fd39b20542841b6e9
Genildo Ferreira <gf.analyst@gmail.com>
2016-05-08 12:10:30 -0300

    some file structure changes

--- app/do_shdsl_alarms.bat
+++ app/do_shdsl_alarms.bat
@@ -1,1 +1,1 @@
-\%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/shdsl_alarms.rb
+%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/shdsl_alarms.rb
