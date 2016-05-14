2712552820a8ddc84541f99b6e7c57ef2afd2407
Genildo Ferreira <gf.analyst@gmail.com>
2016-05-14 17:26:14 -0300

    some bug fixes

---
+++ app/infrastructure_alarms_to_log.bat
@@ -0,0 +1,1 @@
+%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/infrastructure_alarms_to_log.rb
--- app/infrastructure_to_log.bat
+++
@@ -1,1 +0,0 @@
-%JRUBY_HOME%\bin\jruby.exe -e $stdout.sync=true;$stderr.sync=true;load($0=ARGV.shift) C:/dev/Projects/nws/app/infrastructure_alarms_loader.rb
